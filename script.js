4a4bcc57f5f1b6330f42ee9465eff450eeaa95fc
Yuichiro1307 <yuichirou20130707@gmail.com>
2025-04-24 20:12:54 +0900

    Update script.js

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ function play() {
 
   switch (version) {
     case "1.12.2":
-      url = "https://your-github-user.github.io/eagler-1.12.2/";
+      url = "1.12.2/";
       break;
     case "1.8.8":
-      url = "https://your-github-user.github.io/eagler-1.8.8/";
+      url = "1.8.8/";
       break;
     case "1.5.2":
-      url = "https://your-github-user.github.io/eagler-1.5.2/";
+      url = "1.5.2/";
       break;
   }
 
